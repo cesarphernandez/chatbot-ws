889e90892fa8f49cd352081c9ef795ba5cc53abf
fibiaan <fibianmejia@hotmail.com>
2024-08-27 21:50:08 -0500

    feat(styling): tailwind developing concept

--- code/views/error/404.php
+++ code/views/error/404.php
@@ -3,33 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Not Found</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            padding: 50px;
-        }
-
-        h1 {
-            font-size: 48px;
-            margin-bottom: 20px;
-        }
-
-        p {
-            font-size: 18px;
-            margin-bottom: 20px;
-        }
-
-        a {
-            color: #007BFF;
-            text-decoration: none;
-        }
-    </style>
+    <title>We dont know that page</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-<h1>404 Not Found</h1>
-<p>Sorry, the page you are looking for could not be found.</p>
-<p><a href="/">Go to Homepage</a></p>
+<body class="w-screen h-screen flex flex-wrap justify-center items-center">
+    <div class="w-3/5 flex flex-wrap justify-center items-center">
+        <h1 class="w-full text-center text-5xl py-2">
+            Page Not Found
+        </h1>
+        <p class="w-full text-center py-2">
+            Sorry, the page you are looking is not in our contacts yet :(
+        </p>
+        <p class=" py-2">
+            <a class="border rounded-md px-3 py-2" href="/">
+                Go where you know
+            </a>
+        </p>
+    </div>
 </body>
 </html>
